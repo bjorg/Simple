03eda71a87399c3254a61f0fbaa8ac9fb53ae5e5
Steve Bjorg <steve.bjorg@gmail.com>
2014-05-27 13:02:23 -0700

    Adding Text.hs with simplified text functionality

---
+++ Simple/Text.hs
@@ -0,0 +1,210 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+{- String Methods from .Net
+
+GetHashCode
+Insert
+Join -> intercalate
+PadLeft
+PadRight
+Remove
+Split
+Substring
+
+Additions:
++ getHashCodeIgnoreCase
++ escape
++ lastIndexOfIgnoreCase
++ quote
++ repeatPattern
++ replaceAll
++ replaceWithinDelimiters
++ unescape
+
+Not Implemented from .Net:
+* Format
+* IsNormalized
+* Normalize
+* CompareOrdinal
+
+-}
+
+module Simple.String (
+    Text,
+    ToText,
+    ToString,
+    appendChar,
+    appendText,
+    isEmptyOrWhitespaceText,
+    isEmptyText,
+    lengthText,
+    prependChar,
+    replaceText,
+    toLowerText,
+    toUpperText,
+    trimText,
+    trimStartText,
+    trimEndText,
+    compareText,
+    compareIgnoreCaseText,
+    containsText,
+    containsIgnoreCaseText,
+    equalsText,
+    equalsIgnoreCaseText,
+    startsWithText,
+    startsWithIgnoreCaseText,
+    endsWithText,
+    endsWithIgnoreCaseText,
+    indexOfAnyChar,
+    indexOfAnyIgnoreCaseChar,
+    indexOfChar,
+    indexOfIgnoreCaseChar,
+    indexOfText,
+    indexOfIgnoreCaseText,
+    lastIndexOfAnyChar,
+    lastIndexOfAnyIgnoreCaseChar,
+    lastIndexOfChar,
+    lastIndexOfIgnoreCaseChar,
+    lastIndexOfText,
+    lastIndexOfIgnoreCaseText
+) where
+    
+import qualified Data.Char as C
+import qualified Data.Text as T
+
+type Text = T.Text
+
+-- Conversions
+class ToText a where
+    toText :: a -> Text
+
+instance ToText Char where
+    toText = T.singleton
+
+instance ToText String where
+    toText = T.pack
+
+{- This type class should really be in Data.String! -}
+class ToString a where
+    toString :: a -> String
+
+instance ToString Text where
+    toString = T.unpack
+
+-- Basic interface
+
+appendChar :: Text -> Char -> Text
+appendChar = T.snoc
+
+appendText :: Text -> Text -> Text
+appendText = T.append
+
+isEmptyOrWhitespaceText :: Text -> Bool
+isEmptyOrWhitespaceText text = isEmptyText $ trimText text
+
+isEmptyText :: Text -> Bool
+isEmptyText = T.null
+
+lengthText :: Text -> Int
+lengthText = T.length
+
+prependChar :: Text -> Char -> Text
+prependChar text char = T.cons char text
+
+-- Transformations
+
+replaceText :: Text -> (Text, Text) -> Text
+replaceText text (find, replace) = T.replace find replace text
+
+toLowerText :: Text -> Text
+toLowerText = T.toLower
+
+toUpperText :: Text -> Text
+toUpperText = T.toUpper
+
+trimText :: Text -> Text
+trimText = T.strip
+
+trimStartText :: Text -> Text
+trimStartText = T.stripStart
+
+trimEndText :: Text -> Text
+trimEndText = T.stripEnd
+
+-- Comparisons
+compareText :: Text -> Text -> Ordering
+compareText = compare
+
+compareIgnoreCaseText :: Text -> Text -> Ordering
+compareIgnoreCaseText left right = (T.toCaseFold left) `compare` (T.toCaseFold right)
+
+containsText :: Text -> Text -> Bool
+containsText text value = T.isInfixOf value text
+
+containsIgnoreCaseText :: Text -> Text -> Bool
+containsIgnoreCaseText text value = T.isInfixOf (T.toCaseFold value) (T.toCaseFold text)
+
+equalsText :: Text -> Text -> Bool
+equalsText = (==)
+
+equalsIgnoreCaseText :: Text -> Text -> Bool
+equalsIgnoreCaseText left right = T.toCaseFold left == T.toCaseFold right
+
+startsWithText :: Text -> Text -> Bool
+startsWithText text prefix = T.isPrefixOf prefix text
+
+startsWithIgnoreCaseText :: Text -> Text -> Bool
+startsWithIgnoreCaseText text prefix = T.isPrefixOf (T.toCaseFold prefix) (T.toCaseFold text)
+
+endsWithText :: Text -> Text -> Bool
+endsWithText text suffix = T.isSuffixOf suffix text
+
+endsWithIgnoreCaseText :: Text -> Text -> Bool
+endsWithIgnoreCaseText text suffix = T.isSuffixOf (T.toCaseFold suffix) (T.toCaseFold text)
+
+-- Searching
+
+indexOfAnyChar :: Text -> [Char] -> Maybe Int
+indexOfAnyChar text chars = T.findIndex (`elem` chars) text
+
+indexOfAnyIgnoreCaseChar :: Text -> [Char] -> Maybe Int
+indexOfAnyIgnoreCaseChar text chars = T.findIndex (`elem` (map C.toLower chars)) (T.toLower text)
+
+indexOfChar :: Text -> Char -> Maybe Int
+indexOfChar text char = T.findIndex (char==) text
+
+indexOfIgnoreCaseChar :: Text -> Char -> Maybe Int
+indexOfIgnoreCaseChar text char = T.findIndex ((C.toLower char)==) (T.toLower text)
+
+indexOfText :: Text -> Text -> Maybe Int
+indexOfText text value = if T.null match then Nothing else (Just $ lengthText prefix)
+    where (prefix, match) = T.breakOn value text
+
+indexOfIgnoreCaseText :: Text -> Text -> Maybe Int
+indexOfIgnoreCaseText text value = (T.toCaseFold text) `indexOfText` (T.toCaseFold value)
+
+lastIndexOfAnyChar :: Text -> [Char] -> Maybe Int
+lastIndexOfAnyChar text chars = reverseIndex text 1 $ T.findIndex (`elem` chars) (T.reverse text)
+
+lastIndexOfAnyIgnoreCaseChar :: Text -> [Char] -> Maybe Int
+lastIndexOfAnyIgnoreCaseChar text chars = reverseIndex text 1 $ T.findIndex (`elem` (map C.toLower chars)) (T.toLower $ T.reverse text)
+
+lastIndexOfChar :: Text -> Char -> Maybe Int
+lastIndexOfChar text char = reverseIndex text 1 $ T.findIndex (char==) (T.reverse text)
+
+lastIndexOfIgnoreCaseChar :: Text -> Char -> Maybe Int
+lastIndexOfIgnoreCaseChar text char = reverseIndex text 1 $ T.findIndex ((C.toLower char)==) (T.toLower $ T.reverse text)
+
+lastIndexOfText :: Text -> Text -> Maybe Int
+lastIndexOfText text value = reverseIndex text (T.length value) $ if T.null match then Nothing else (Just $ lengthText prefix)
+    where (prefix, match) = T.breakOn (T.reverse value) (T.reverse text)
+
+lastIndexOfIgnoreCaseText :: Text -> Text -> Maybe Int
+lastIndexOfIgnoreCaseText text value = (T.toCaseFold text) `lastIndexOfText` (T.toCaseFold value)
+
+-- Helpers
+
+reverseIndex :: Text -> Int -> Maybe Int -> Maybe Int
+reverseIndex text matchLength Nothing = Nothing
+reverseIndex text matchLength (Just value) = Just $ T.length text - value - matchLength
