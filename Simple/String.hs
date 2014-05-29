a1f30aa514c9257b2dbab5d8319634ed8c169cd5
Steve Bjorg <steve.bjorg@gmail.com>
2014-05-29 12:23:49 -0700

    added a copyright header

--- Simple/String.hs
+++ Simple/String.hs
@@ -1,3 +1,15 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Simple.String
+-- Copyright   :  See LICENSE file
+-- License     :  Apache 2.0
+--
+-- Maintainer  :  Steve G. Bjorg <steve.bjorg@gmail.com>
+-- Stability   :  experimental
+-- Portability :  portable (not tested)
+--
+-----------------------------------------------------------------------------
+
 module Simple.String (
     ToString,
     toString
